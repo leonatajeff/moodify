4859b1ce0df6e279c278630f47e968ef5f503323
Jefferson Leonata <leonatajeff@gmail.com>
2022-11-06 02:40:39 -0500

    Add test fetch for most recent 20 songs favorited

--- api/app.py
+++ api/app.py
@@ -1,40 +1,49 @@
 from operator import is_
-from flask import Flask, request, url_for, session, redirect, jsonify
+import time
+from flask import Flask, request, json, session, redirect, jsonify
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
-app.secret_key = "19u2h31289ias" # this is random, ignore for now.
-app.config['SESSION_COOKIE_NAME'] = 'test cookie'
-TOKEN_INFO = 'token_info'
+CLIENT_ID=""
+CLIENT_SECRET=""
+REDIRECT_URI="http://localhost:3000"
+PERMISSIONS="user-library-read"
+app.config.update(SECRET_KEY=CLIENT_SECRET)
+sp_oauth = SpotifyOAuth( CLIENT_ID, CLIENT_SECRET,REDIRECT_URI,scope=PERMISSIONS,cache_path='.spotipyoauthcache' )
 
 @app.route('/authorize')
 def login():
-    sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return jsonify({
         'auth_endpoint': auth_url
     })
 
-@app.route('/sentence', methods=['GET'])
-def get_top_songs_genre(song_ids, artist_ids, token_info):
-        
+@app.route('/registerToken', methods=['POST'])
+def register_token():
+    data = json.loads(request.data) 
+    token_info = sp_oauth.get_access_token(data['code'])
+    session['token_info'] = token_info
+    return token_info
+
+def check_token():
+    token_info = session.get('token_info', None)
+    if not token_info:
+        raise 'exception'
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+
+    return token_info
+
+@app.route('/getPrompt', methods=['GET'])
+def get_prompt():
+    token_info = check_token()
     sp = spotipy.Spotify(auth=token_info['access_token'])
-
-    genres = sp.recommendation_genre_seeds()
-    refined_genres = sp.recommendations(song_ids, artist_ids, genres, limit=1)
-    example_string = refined_genres + " abstract" + " digital art"
-    return str(example_string)
-
-def create_spotify_oauth():
-    # ENTER YOUR OWN CLIENT ID AND SECRET FROM THE SPOTIFY DASHBOARD
-    return SpotifyOAuth(
-        client_id='',
-        client_secret='',
-        redirect_uri='http://localhost:3000',
-        scope='user-library-read'
-    )
-
-# make sure to delete client secret and client id before uploading to Github or anywhere public
-# https://app-engine-react-demo-dot-cs1520-jel211.ue.r.appspot.com/
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+    return results
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 // 'URLSearchParams(window.location.search)' will get url string after the '?' & .get() will get the code value from the url
 const code = new URLSearchParams(window.location.search).get('code')
-
+let responseCode = {
+    "code": code,
+}
 export default function Home() {
 
     const handleLogin = async () => {
@@ -17,8 +19,11 @@ export default function Home() {
             <h2 className="moodify-subheader">
                 What if you can see your favorite sounds?
             </h2>
-            {code ? <img style={{ width: 400, height: 400 }} src="https://www.kurin.com/wp-content/uploads/placeholder-square.jpg" alt='placeholding' /> :
-                
+            {code ? <div>
+                    <img style={{ width: 400, height: 400 }} src="https://www.kurin.com/wp-content/uploads/placeholder-square.jpg" alt='placeholding' /> 
+                    {console.log(axios.post('/registerToken', responseCode).then(axios.get('/getPrompt')))}
+                </div> :
+
                 <button className="spotify-button" onClick={handleLogin}>
                     <text className="login-text"> LOGIN WITH SPOTIFY </text>
                 </button>}
