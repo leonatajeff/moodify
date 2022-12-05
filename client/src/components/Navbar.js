2beb3ce9cd94257a57698d4a688d68dc57d292be
Jefferson Leonata <leonatajeff@gmail.com>
2022-12-05 10:02:19 -0500

    Add dono and update styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,9 +4,14 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/About">About</NavLink>
-      <NavLink to="/Community">Community</NavLink>
+      <div className="navbar-text">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/About">About</NavLink>
+        <NavLink to="/Community">Community</NavLink>
+      </div>
+      <div className="donate">
+        <NavLink className="donate-button" to="/Donate">Donate</NavLink>  
+      </div>
     </div>
   );
 }
